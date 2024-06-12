da03266876fed0d405ab555de89b3fbba0e57a5c
rmramos <rmramos2399@gmail.com>
2024-06-12 11:26:36 +0800

    added few more functions

--- try.c
+++ try.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    int val1, val2, result;
+    int val1, val2, resultadd, resultsub, resultmul;
+    float resultdiv;
+    //add
     scanf("%d %d" , &val1, &val2);
-    result = val1 + val2;
-    printf("\n&d" , result);
+    resultadd = val1 + val2;
+    
+    //sub
+    resultsub = val1 - val2;
+    //mul
+    resultmul = val1 * val2;
+    //div 2
+    resultdiv = val1/val2;
+    printf("\n SUM: &d" , resultadd);
+    printf("\n DIF: &d" , resultsub);
+    printf("\n PROD: &d" , resultmul);
+    printf("\n QUO: &d" , resultdiv);
     return 0;
 }
