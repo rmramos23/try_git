f0bb6648107f94f65cf1e169c1af0227df91a5c1
rmramos <rmramos2399@gmail.com>
2024-06-12 09:03:25 +0800

    added feature

--- try.c
+++ try.c
@@ -3,16 +3,16 @@
 int main() {
     char ch;
     char s[50];
-    char sen[100]; // Adjust size as needed
-
+    char sen[99]; // Adjust size as needed
+    
     scanf("%c\n", &ch);
     scanf ("%s\n", &s);
-
+    //
     scanf("%[^\n]%*c", sen);
 
     printf("%c\n", ch);
     printf("%s\n", s);
     printf("%s\n", sen);
-   
+    printf("try\n");
     return 0;
 }
