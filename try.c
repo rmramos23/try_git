133a93bb11427da998bd3aa200e8f32b160e7c06
rmramos <rmramos2399@gmail.com>
2024-06-12 11:10:18 +0800

    scratch

--- try.c
+++ try.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    char ch;
-    char s[50];
-    char sen[99]; // Adjust size as needed
-    
-    scanf("%c\n", &ch);
-    scanf ("%s\n", &s);
-    //
-    scanf("%[^\n]%*c", sen);
 
-    printf("%c\n", ch);
-    printf("%s\n", s);
-    printf("%s\n", sen);
     return 0;
 }
